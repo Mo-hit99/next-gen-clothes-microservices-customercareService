07b93f7c685e0497ed61e34144c7ca4881de64ab
EvolMohit <125653626+EvolMohit@users.noreply.github.com>
2024-11-27 21:09:03 +0530

    routes chnges

--- config/db_connection/redis_connection.js
+++ config/db_connection/redis_connection.js
@@ -1,7 +1,11 @@
 import redis from 'redis'
 
 const redisClient = redis.createClient({
-    url: 'redis://localhost:6379'
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
 })
 
 redisClient.on('error',(error)=>{
